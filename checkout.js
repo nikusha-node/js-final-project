199c2fb60c937153e74e8e7917c3af5ef88fdec0
Niku2009sha <nikusha.chigladze2009@gmail.com>
2025-11-02 11:41:45 +0400

    test commit

--- checkout.js
+++ checkout.js
@@ -1,10 +1,10 @@
 window.addEventListener("scroll", function() {
-  const header = document.querySelector(".header-main");
-  if (window.scrollY > 0) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
+    const header = document.querySelector(".fixed");
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
 
 // გადახდის ფორმა
